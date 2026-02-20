06c9c8e21719c00508087856dc67785a801bab8c
vivianediasnunes097-a11y <vivianediasnunes097@gmail.com>
2026-02-20 08:44:23 -0300

    Primeiro commit

---
+++ hello/src/App.jsx
@@ -0,0 +1,27 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+export function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <body>
+      <main>
+          <h1>Login</h1>
+          <p>Digite seu email e senha para acessar sua conta</p>
+          <form>
+          <input type="email" placeholder="Seu email" />
+          <input type="password" placeholder="Sua senha" />
+          <button>Entrar</button>
+          </form>
+      </main>
+      </body>
+    </>
+  )
+}
+
+
+
